2911976738e6296a7d144fec9d8ac1a72c23bfb4
Akshata MAdavi <akshatamadavi@gmail.com>
2025-09-23 20:28:30 -0700

    final code

---
+++ main.py
@@ -0,0 +1,17 @@
+from autogluon.tabular import TabularDataset, TabularPredictor
+
+# 1. Load data (Titanic dataset as example)
+train_data = TabularDataset("https://autogluon.s3.amazonaws.com/datasets/titanic/train.csv")
+test_data  = TabularDataset("https://autogluon.s3.amazonaws.com/datasets/titanic/test.csv")
+
+# The column we want to predict:
+label = "Survived"
+
+# 2. Create predictor and train
+predictor = TabularPredictor(label=label).fit(train_data)
+
+# 3. Make predictions
+preds = predictor.predict(test_data)
+
+# 4. Evaluate (if ground truth is available)
+# predictor.evaluate(test_data)
