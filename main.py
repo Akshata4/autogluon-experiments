d7abe06be1c474a3a9c254c287035fa3e43cbf09
Akshata MAdavi <akshatamadavi@gmail.com>
2025-09-24 12:22:04 -0700

    autogluon used for titanic dataset

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
+import pandas as pd
 
 # 1. Load data (Titanic dataset as example)
-train_data = TabularDataset("https://autogluon.s3.amazonaws.com/datasets/titanic/train.csv")
-test_data  = TabularDataset("https://autogluon.s3.amazonaws.com/datasets/titanic/test.csv")
+# train_data = TabularDataset("https://autogluon.s3.amazonaws.com/datasets/titanic/train.csv")
+# test_data  = TabularDataset("https://autogluon.s3.amazonaws.com/datasets/titanic/test.csv")
 
-# The column we want to predict:
-label = "Survived"
+# # The column we want to predict:
+# label = "Survived"
+
+train_data = pd.read_csv('data/train.csv')
+test_data = pd.read_csv('data/test.csv')
+label = 'Survived'
 
 # 2. Create predictor and train
 predictor = TabularPredictor(label=label).fit(train_data)
@@ -14,4 +19,4 @@ predictor = TabularPredictor(label=label).fit(train_data)
 preds = predictor.predict(test_data)
 
 # 4. Evaluate (if ground truth is available)
-# predictor.evaluate(test_data)
+predictor.evaluate(test_data)
