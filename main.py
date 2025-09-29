48446af7ceb1704a7cd131869db78eaf557451eb
Akshata Madavi <Akshata.madavi@sjsu.edu>
2025-09-29 12:12:54 -0700

    uv setup done

--- main.py
+++ main.py
@@ -12,8 +12,43 @@ train_data = pd.read_csv('data/train.csv')
 test_data = pd.read_csv('data/test.csv')
 label = 'Survived'
 
+# Function to split Name column
+def split_name_column(df):
+	name_split = df['Name'].str.split(',', expand=True)
+	df['Lastname'] = name_split[0].str.strip()
+	title_first = name_split[1].str.strip().str.split('.', expand=True)
+	df['Title'] = title_first[0].str.strip()
+	df['Firstname'] = title_first[1].str.strip()
+	df['Title'] = df['Title'].replace({
+    'Mlle':'Miss','Ms':'Miss','Mme':'Mrs','Lady':'Rare','Countess':'Rare','Sir':'Rare',
+    'Jonkheer':'Rare','Don':'Rare','Dona':'Rare','Capt':'Rare','Col':'Rare','Major':'Rare','Rev':'Rare','Dr':'Rare'
+})
+	return df
+
+# Split and save train
+train_data = split_name_column(train_data)
+
+## New features
+train_data['FamilySize'] = train_data['SibSp'] + train_data['Parch'] + 1
+train_data['IsAlone'] = (train_data['FamilySize'] == 1).astype(int)
+
+ticket_counts = train_data['Ticket'].value_counts()
+train_data['TicketGroupSize'] = train_data['Ticket'].map(ticket_counts)
+
+
+
+# Split and save test
+test_data = split_name_column(test_data)
+
 # 2. Create predictor and train
-predictor = TabularPredictor(label=label).fit(train_data)
+predictor = TabularPredictor(label=label, problem_type='binary', eval_metric='accuracy').fit(train_data=train_data,
+    presets='best_quality',                 # stronger than 'medium_quality'
+    time_limit=1800,                        # e.g., 30 minutes
+    num_bag_folds=5,                        # internal CV bagging
+    num_bag_sets=1,
+    num_stack_levels=1,                     # try 1 → 2 if you have time
+    ag_args_fit={'verbosity': 1})
+# predictor = TabularPredictor(label=label).fit(train_data)
 
 # 3. Make predictions
 preds = predictor.predict(test_data)
--- predict.py
+++ predict.py
@@ -2,10 +2,23 @@ import pandas as pd
 from autogluon.tabular import TabularPredictor
 
 # === Config ===
-MODEL_PATH = "AutogluonModels/ag-20250924_191554"   # replace with your model folder
+MODEL_PATH = "AutogluonModels/ag-20250928_231126"   # replace with your model folder
 TEST_PATH  = "data/test.csv"                         # your test dataset
 OUTPUT_PATH = "data/submission.csv"                  # where to save predictions
-PRED_COLUMN = "PredictedLabel"                  # new column for predictions
+PRED_COLUMN = "Survived"                  # new column for predictions
+
+# Function to split Name column
+def split_name_column(df):
+	name_split = df['Name'].str.split(',', expand=True)
+	df['Lastname'] = name_split[0].str.strip()
+	title_first = name_split[1].str.strip().str.split('.', expand=True)
+	df['Title'] = title_first[0].str.strip()
+	df['Firstname'] = title_first[1].str.strip()
+	df['Title'] = df['Title'].replace({
+    'Mlle':'Miss','Ms':'Miss','Mme':'Mrs','Lady':'Rare','Countess':'Rare','Sir':'Rare',
+    'Jonkheer':'Rare','Don':'Rare','Dona':'Rare','Capt':'Rare','Col':'Rare','Major':'Rare','Rev':'Rare','Dr':'Rare'
+})
+	return df
 
 # === Load model ===
 predictor = TabularPredictor.load(MODEL_PATH)
@@ -14,17 +27,27 @@ predictor = TabularPredictor.load(MODEL_PATH)
 # ensemble_name = lb.iloc[0]["model"]  # usually the top row is the WeightedEnsemble_* best model
 # print("Using model:", ensemble_name)
 
+
+
 # === Load test data ===
-test_df = pd.read_csv(TEST_PATH)
+test_data = pd.read_csv(TEST_PATH)
+test_data = split_name_column(test_data)
+
+## New features
+test_data['FamilySize'] = test_data['SibSp'] + test_data['Parch'] + 1
+test_data['IsAlone'] = (test_data['FamilySize'] == 1).astype(int)
+
+ticket_counts = test_data['Ticket'].value_counts()
+test_data['TicketGroupSize'] = test_data['Ticket'].map(ticket_counts)
 
 # === Make predictions ===
-preds = predictor.predict(test_df)
+preds = predictor.predict(test_data)
 
 # === Save results ===
 # Add new column to test dataframe
-test_df[PRED_COLUMN] = preds
+test_data[PRED_COLUMN] = preds
 
 # Save to CSV
-test_df[['PassengerId', PRED_COLUMN]].to_csv(OUTPUT_PATH, index=False)
+test_data[['PassengerId', PRED_COLUMN]].to_csv(OUTPUT_PATH, index=False)
 
 print(f"✅ Predictions saved to {OUTPUT_PATH} with column '{PRED_COLUMN}'")
